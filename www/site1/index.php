528b9b5881f5affe59294152f583e22c6d2ea576
xianyunyhe <tianlei542475736@gmail.com>
2018-09-19 16:28:30 +0800

    修改demo

--- www/site1/index.php
+++ www/site1/index.php
@@ -1,3 +1,20 @@
 <?php
-echo 'Site 1<br />';
-phpinfo();
+$redis = new Redis();
+
+try {
+    $redis->connect("redis","6379");
+} catch (Exception $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+//phpinfo();
+$dsn = 'mysql:dbname=mysql;host=mysql';
+$user = 'root';
+$password = '123456';
+
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+   echo 'Connection failed: ' . $e->getMessage();
+}
